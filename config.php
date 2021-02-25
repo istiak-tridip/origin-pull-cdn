92d91d79e21d3f1435ed68d315d238266204efdb
Istiak Tridip <khantridip88@gmail.com>
2021-02-25 20:23:58 +0600

    feat: Add base uri & timeout config for origin

--- config.php
+++ config.php
@@ -1,9 +1,20 @@
 <?php
 
 return [
-    "origin" => "istiaktridip.net",
+    "origin" => [
+        // Base URI is used with relative requests
+        "base_uri" => "https://istiaktridip.net",
+
+        // Float describing the total timeout of the request in seconds.
+        // Use 0 to wait indefinitely.
+        "timeout"  => 5.0,
+    ],
     "cache"  => [
-        "lifetime"  => 0,
+        // Absolute Path of the directory where cache entries will be stored.
         "directory" => __DIR__ . "/storage",
+
+        // The lifetime in number of seconds for a cache entry.
+        // If zero (the default), the entry never expires.
+        "lifetime"  => 0,
     ],
 ];
